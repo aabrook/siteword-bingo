415f1659727a86efbcf0561ac36bdeb4948e24e6
aabrook <aabrook@gmail.com>
2018-04-11 20:33:14 +1000

    Added sets 9 - 12

--- src/lib/words.js
+++ src/lib/words.js
@@ -6,5 +6,9 @@ export default () => [
   ['she', 'on', 'went', 'like', 'in', 'down'],
   ['got', 'me', 'the', 'comes', 'a', 'said'],
   ['put', 'go', 'comes', 'some', 'went', 'we', 'like'],
-  ['where', 'his', 'said', 'my', 'this', 'comes']
+  ['where', 'his', 'said', 'my', 'this', 'comes'],
+  ['see', 'Dad', 'is', 'in', 'said', 'one', 'down', 'like', 'and', 'two', 'a', 'he', 'up', 'the', 'she', 'on', 'comes', 'me', 'went', 'got', 'Mum', 'I', 'look', 'here', 'am', 'big', 'little'],
+  ['said', 'put', 'we', 'go', 'where', 'some'],
+  ['my', 'her', 'this', 'one', 'his', 'has'],
+  ['all', 'put', 'this', 'can', 'did', 'been', 'some']
 ]

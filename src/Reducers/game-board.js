9daebe24a05a0a85197c0e452d8b9eb285a83a75
aabrook <aabrook@gmail.com>
2018-04-15 09:29:20 +1000

    Add button to create game cards

--- src/Reducers/game-board.js
+++ src/Reducers/game-board.js
@@ -1,4 +1,22 @@
-export default ({
+import { remove } from 'ramda'
+
+const { floor, random } = Math
+
+const words = (total, xs) => {
+  if(!xs || total <= 0 || !xs.length){
+    return []
+  }
+
+  const idx = floor(random() * floor(xs.length - 1))
+  return [xs[idx], ...words(total - 1, remove(idx, 1, xs))]
+}
+
+export default {
   enterRowCount: ({ count }, state) => ({ ...state, rowCount: count }),
-  enterColumnCount: ({ count }, state) => ({ ...state, colCount: count })
-})
+  enterColumnCount: ({ count }, state) => ({ ...state, colCount: count }),
+  refreshCard: (_, state) => {
+    const { rowCount, colCount, activeWords } = state
+
+    return { ...state, gameCard: words(rowCount * colCount, activeWords) }
+  }
+}
